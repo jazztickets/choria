5f2dea1ef2fb99608cc6612adc005aee9e6204e2
Alan Witkowski <alan.witkowski@gmail.com>
2015-12-06 19:52:43 -0700

    fixed atlas generation

--- assets/tiles/make_atlas.sh
+++ assets/tiles/make_atlas.sh
@@ -4,12 +4,12 @@ mkdir -p out
 rm -f atlas.png
 
 for f in textures/*.png; do
-	convert "$f" -background none -write mpr:tile +delete -size 34x34 -tile-offset -1-1 tile:mpr:tile "out/t_`basename \"$f\"`"
+	convert "$f" -compose copy -write mpr:tile +delete -size 34x34 -tile-offset -1-1 tile:mpr:tile "out/t_`basename \"$f\"`"
 done
 
 count=$(ls textures/*.png | wc -l)
 cols=$(echo $count | awk '{ x = sqrt($1); print x%1 ? int(x)+1 : x}')
 
-montage -background transparent -geometry 34x34 -tile ${cols}x out/* atlas.png
+montage -background transparent -geometry 34x34 -tile ${cols}x out/* atlas0.png
 
 rm -rf out/
