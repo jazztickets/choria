81e1323fc2e10f78ea9f10cca3b09b46d8f39192
Alan Witkowski <alan.witkowski@gmail.com>
2015-12-21 16:38:34 -0700

    removed old web tool

--- assets/webtool/connection.php
+++
@@ -1,8 +0,0 @@
-<?
-$DatabaseFile = "sqlite:../../working/stats/stats.db";
-$Database = new PDO($DatabaseFile);
-if(!$Database) {
-	die("Cannot open database");
-}
-$Database->query("PRAGMA foreign_keys=ON");
-?>
--- assets/webtool/index.php
+++
@@ -1,7 +0,0 @@
-<?
-	include("topinclude.php");
-?>
-
-<?
-	include("footer.php");
-?>
