6a4b5557bd789038013969ff97fe1888fc853e4a
Alan Witkowski <alan.witkowski@gmail.com>
2015-12-21 20:36:44 -0700

    fixed design tool bug added default value for scope

--- assets/web/js/edit.js
+++ assets/web/js/edit.js
@@ -13,49 +13,20 @@ $(document).ready(function() {
 	var container = document.getElementById('sheet');
 	data_url = "/data" + querystring;
 	$.getJSON(data_url, function(response) {
-		var column_names = response['column_names'];
 		var data = response['data'];
 		var message = response['message'];
+		column_names = response['column_names'];
 		references = response['references'];
 		if(message != undefined) {
 			$('#message').html(message);
 			return;
 		}
 
-		// build autocomplete data and change data array
+		// Build dropdown data
 		var columns = [];
-		for(var i in column_names) {
-			var column = column_names[i];
+		transform_data(data, columns);
 
-			// Check for foreign key reference
-			if(references.hasOwnProperty(column)) {
-				var reference = references[column];
-				var names = []
-
-				// Build drop down
-				for(var key in reference) {
-					if(reference.hasOwnProperty(key)) {
-						var value = reference[key] + " (" + key + ")";
-						names.push(value);
-					}
-				}
-
-				// Add to columns list
-				columns.push({
-					type: 'dropdown',
-					source: names,
-				});
-
-				// Change data
-				for(var data_row in data) {
-					id = data[data_row][i];
-					data[data_row][i] = reference[id] + " (" + id + ")";
-				}
-			}
-			else
-				columns.push({});
-		}
-
+		// Create spreadsheet
 		hot = new Handsontable(container, {
 			data: data,
 			rowHeaders: true,
@@ -138,10 +109,51 @@ function update_buttons(state) {
 	$('#delete_id').prop('disabled', !state);
 }
 
+// Transform data set by modifying foreign key values to "Name (ID)"
+function transform_data(data, columns) {
+
+	// build autocomplete data and change data array
+	for(var i in column_names) {
+		var column = column_names[i];
+
+		// Check for foreign key reference
+		if(references.hasOwnProperty(column)) {
+			var reference = references[column];
+			var names = []
+
+			// Build drop down
+			for(var key in reference) {
+				if(reference.hasOwnProperty(key)) {
+					var value = reference[key] + " (" + key + ")";
+					names.push(value);
+				}
+			}
+
+			// Add to columns list
+			columns.push({
+				type: 'dropdown',
+				source: names,
+			});
+
+			// Change data
+			for(var data_row in data) {
+				var id = data[data_row][i];
+				data[data_row][i] = reference[id] + " (" + id + ")";
+			}
+		}
+		else
+			columns.push({});
+	}
+}
+
 // Reload data
 function reload() {
 	$.getJSON(data_url, function(response) {
-		hot.loadData(response['data']);
+		var data = response['data'];
+		var columns = [];
+		transform_data(data, columns)
+
+		hot.loadData(data);
 		update_buttons(1);
 	});
 }
