249e556f0a5072b5665e2074ed5f40b092df7218
Alan Witkowski <alan.witkowski@gmail.com>
2015-12-27 15:39:34 -0700

    added more items

--- assets/art/make_pngs.sh
+++ assets/art/make_pngs.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 file=$1
+if [ -z "$file" ]; then
+	echo "No input .svg file"
+	exit
+fi
+
+# get list of objects
 names=`grep "title[0-9]" "$file" | egrep ">[a-z_]+<" -o | tr -d '<>'`
 
+# export pngs
 convert -density 45 -background none $file -crop 32x32 -depth 8 +repage PNG32:export/_out.png
 
+# name files
 i=0
 for name in $names; do
 	oldname="export/_out-${i}.png"
