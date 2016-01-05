d7adcf0b35209e24af5cd7a9731442f3db673485
Alan Witkowski <alan.witkowski@gmail.com>
2016-01-04 17:43:04 -0700

    moved atlas dir

--- assets/art/make_atlas.sh
+++ assets/art/make_atlas.sh
@@ -3,11 +3,11 @@
 mkdir -p out
 rm -f atlas.png
 
-for f in textures/*.png; do
+for f in tiles/*.png; do
 	convert "$f" -compose copy -write mpr:tile +delete -size 66x66 -tile-offset -1-1 tile:mpr:tile "out/t_`basename \"$f\"`"
 done
 
-count=$(ls textures/*.png | wc -l)
+count=$(ls tiles/*.png | wc -l)
 cols=$(echo $count | awk '{ x = sqrt($1); print x%1 ? int(x)+1 : x}')
 
 montage -background transparent -geometry 66x66 -tile ${cols}x out/* atlas0.png
