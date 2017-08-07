96d4eb1ad417ff8eaa97c583bdff1723a144dfb5
Alan Witkowski <alan.witkowski@gmail.com>
2017-08-06 22:47:38 -0600

    update gpl header

--- src/random.cpp
+++ src/random.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 * choria - https://github.com/jazztickets/choria
-* Copyright (C) 2016  Alan Witkowski
+* Copyright (C) 2017  Alan Witkowski
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
