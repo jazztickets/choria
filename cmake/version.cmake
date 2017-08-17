9d51f6083acf2c284223e64637d3e88705cacbf9
Alan Witkowski <alan.witkowski@gmail.com>
2017-08-17 16:27:39 -0600

    small cleanup

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,4 +1,4 @@
-execute_process(COMMAND bash "-c" "git log --oneline | wc -l" OUTPUT_VARIABLE BUILD_NUMBER)
+execute_process(COMMAND git log --oneline COMMAND wc -l OUTPUT_VARIABLE BUILD_NUMBER)
 if(BUILD_NUMBER STREQUAL "")
 	set(BUILD_NUMBER 0)
 endif()
