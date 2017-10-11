49edfbe366cd4052caaf56f8e5322c7155e343a6
Alan Witkowski <alan.witkowski@gmail.com>
2017-10-11 16:05:39 -0600

    removed manager.cpp

--- src/ae/manager.h
+++ src/ae/manager.h
@@ -23,6 +23,8 @@
 #include <ae/type.h>
 #include <unordered_map>
 #include <list>
+#include <limits>
+#include <stdexcept>
 
 // Classes
 template<class T> class _Manager {
@@ -52,3 +54,105 @@ template<class T> class _Manager {
 		NetworkIDType NextID;
 
 };
+
+// Constructor
+template <class T>
+_Manager<T>::_Manager() :
+	NextID(0) {
+
+}
+
+// Destructor
+template <class T>
+_Manager<T>::~_Manager() {
+
+	for(auto &Object : Objects)
+		delete Object;
+}
+
+// Update
+template <class T>
+void _Manager<T>::Update(double FrameTime) {
+
+	// Update objects
+	for(auto Iterator = Objects.begin(); Iterator != Objects.end(); ) {
+		T *Object = *Iterator;
+
+		// Update the object
+		Object->Update(FrameTime);
+
+		// Move deleted objects to deleted list
+		if(Object->Deleted) {
+			DeleteList.push_back(Object);
+			Iterator = Objects.erase(Iterator);
+		}
+		else {
+			++Iterator;
+		}
+	}
+
+	// Delete objects
+	for(auto &Object : DeleteList) {
+		IDMap[Object->NetworkID] = nullptr;
+		delete Object;
+	}
+
+	DeleteList.clear();
+}
+
+// Generate object with new network id
+template <class T>
+T *_Manager<T>::Create() {
+
+	// Search for an empty slot
+	for(NetworkIDType i = 0; i <= std::numeric_limits<NetworkIDType>::max(); i++) {
+		if(!IDMap[NextID]) {
+			T *Object = new T;
+			Object->NetworkID = NextID;
+
+			Objects.push_back(Object);
+			IDMap[NextID] = Object;
+
+			return Object;
+		}
+
+		NextID++;
+	}
+
+	throw std::runtime_error("Ran out of object ids");
+}
+
+// Create object with existing id
+template <class T>
+T *_Manager<T>::CreateWithID(NetworkIDType ID) {
+	T *Object = new T;
+	Object->NetworkID = ID;
+
+	Objects.push_back(Object);
+	IDMap[ID] = Object;
+
+	return Object;
+}
+
+// Get object from manager
+template <class T>
+T *_Manager<T>::GetObject(NetworkIDType ID) {
+
+	auto Iterator = IDMap.find(ID);
+	if(Iterator == IDMap.end())
+		return nullptr;
+
+	return Iterator->second;
+}
+
+// Delete all objects and reset
+template <class T>
+void _Manager<T>::Clear() {
+
+	for(auto Object : Objects)
+		delete Object;
+
+	IDMap.clear();
+	Objects.clear();
+	NextID = 0;
+}
