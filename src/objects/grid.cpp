f12414a384395138f0650b70257c9b9f7571db2b
Alan Witkowski <alan.witkowski@gmail.com>
2018-09-13 10:11:16 -0600

    updated headers

--- src/objects/grid.cpp
+++ src/objects/grid.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
-* choria
-* Copyright (C) 2017  Alan Witkowski
+* choria - https://github.com/jazztickets/choria
+* Copyright (C) 2018  Alan Witkowski
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
