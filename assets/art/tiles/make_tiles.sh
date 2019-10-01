775dea2074bb3c86cdb7abb87320f8032f965d80
Alan Witkowski <alan.witkowski@gmail.com>
2019-10-01 17:39:52 -0600

    updated tile script

--- assets/art/tiles/make_tiles.sh
+++ assets/art/tiles/make_tiles.sh
@@ -8,8 +8,21 @@ type convert >/dev/null 2>&1 || {
 
 in=$1
 
+if [ -z "$in" ]; then
+	echo "Usage: ./`basename $0` directory"
+	exit
+fi
+
 mkdir -p out
+rm -f out/*.png
 
 for f in $in/*.png; do
-	convert "$f" -compose copy -write mpr:tile +delete -size 66x66 -tile-offset -1-1 tile:mpr:tile "out/`basename \"$f\"`"
+	basename=`basename "$f"`
+	tempname="out/t_`basename \"$f\"`"
+
+	convert "$f" -compose copy -write mpr:tile +delete -size 66x66 -tile-offset -1-1 tile:mpr:tile PNG32:"$tempname"
+	#pngcrush -brute "$tempname" out/"$basename"
+	mv "$tempname" "out/$basename"
 done
+
+rm -f out/t_*.png
