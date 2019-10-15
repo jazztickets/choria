62744b15766082d7d35cb9f075cc557fa1da3de6
Alan Witkowski <alan.witkowski@gmail.com>
2019-10-15 13:34:34 -0600

    updated flicker script

--- working/scripts/lights.lua
+++ working/scripts/lights.lua
@@ -2,10 +2,19 @@
 -- Flicker --
 
 Light_flicker = {}
+Light_flicker.Values = {}
+Light_flicker.Count = 100
+Light_flicker.Speed = 10
+Light_flicker.Min = 0.5
+Light_flicker.Max = 1.0
+for i = 1, Light_flicker.Count do
+	Light_flicker.Values[i] = Random.GetReal(Light_flicker.Min, Light_flicker.Max)
+end
 
 function Light_flicker.Update(self, Light, Time)
 
-	Light.a = Random.GetReal(0.3, 0.7)
+	Index = math.floor(Time * self.Speed) % self.Count
+	Light.a = self.Values[Index + 1]
 
 	return Light
 end
