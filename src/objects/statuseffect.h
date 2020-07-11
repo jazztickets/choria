deeb1bef5f7a981842a1e513887bc58d74e2b260
Alan Witkowski <alan.witkowski@gmail.com>
2020-07-10 18:16:47 -0600

    update header

--- src/objects/statuseffect.h
+++ src/objects/statuseffect.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 * choria - https://github.com/jazztickets/choria
-* Copyright (C) 2019  Alan Witkowski
+* Copyright (C) 2020 Alan Witkowski
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
