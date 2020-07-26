2738662672f60aa096ce44f2734bf97ec8ea242f
Alan Witkowski <alan.witkowski@gmail.com>
2020-07-26 12:59:05 -0600

    fix crash

--- src/objects/statchange.cpp
+++ src/objects/statchange.cpp
@@ -68,7 +68,6 @@ void _StatChange::Unserialize(ae::_Buffer &Data, ae::_Manager<_Object> *Manager)
 	Object = Manager->GetObject(NetworkID);
 	if(!Object) {
 		std::cout << "_StatChange::Unserialize BadObject NetworkID=" << NetworkID << std::endl;
-		return;
 	}
 
 	// Get change count
@@ -85,7 +84,8 @@ void _StatChange::Unserialize(ae::_Buffer &Data, ae::_Manager<_Object> *Manager)
 		// Get data
 		if(Type == StatType::BUFF) {
 			uint16_t BuffID = Data.Read<uint16_t>();
-			Values[Type].Pointer = (void *)Object->Stats->BuffsIndex.at(BuffID);
+			if(Object)
+				Values[Type].Pointer = (void *)Object->Stats->BuffsIndex.at(BuffID);
 		}
 		else
 			Values[Type].Integer = Data.Read<int>();
