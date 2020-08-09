843ba2205d9c6dd5e57567a329190e7522ae4ea5
Alan Witkowski <alan.witkowski@gmail.com>
2020-08-08 20:01:41 -0600

    stat updates

--- assets/data/reports/weapons.sh
+++ assets/data/reports/weapons.sh
@@ -2,7 +2,7 @@
 source functions.inc
 
 db="../../../working/stats/stats.db"
-fields="name, cost, maxlevel, mindamage, maxdamage, (mindamage + maxdamage) / 2.0 as avgdamage, printf('%.2f', ((mindamage+maxdamage)/2.0)/cost) as dpc, chance, level, duration, maxhealth, maxmana, healthregen, manaregen, battlespeed, movespeed, evasion, spell_damage"
+fields="name, cost, set_id, maxlevel, mindamage, maxdamage, (mindamage + maxdamage) / 2.0 + pierce as avgdamage, printf('%.2f', ((mindamage+maxdamage)/2.0+pierce)/cost) as dpc, chance, level, duration, maxhealth, maxmana, healthregen, manaregen, battlespeed, movespeed, evasion, spell_damage"
 pre=".headers on \nSELECT $fields FROM item WHERE itemtype_id IN (5, 6, 14)"
 post=" ORDER BY cost, name"
 
