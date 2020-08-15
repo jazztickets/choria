0f5bcd74048bf9db1450e2142c12f6d19b7bc0c9
Alan Witkowski <alan.witkowski@gmail.com>
2020-08-15 10:36:29 -0600

    script update

---
+++ assets/data/graphs/purchase.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+grep PURCHASE "$1" | grep "[0-9]\+x\ [^(]*" -o | sort | uniq -c | gawk 'BEGIN{FS=OFS=" "}{item=$0; sub($1 " " $2, "", item); sub("^ *", "", item); add=$1;if($2 == "5x") { add = $1 * 5; }  sum[item]+=add; } END { for(item in sum) { print sum[item], item}}' | sort -n
