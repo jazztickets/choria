bf7cc8860e1d91f820c4b56598ac6ccade8f6b7f
Alan Witkowski <alan.witkowski@gmail.com>
2020-09-24 08:45:54 -0600

    script update

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-build_type="Release"
 
-mkdir -p build/release
-pushd build/release
+# get type
+build_type="Release"
+if [ -n "$1" ]; then
+	build_type="$1"
+fi
+
+# get build dir
+build_dir=$(echo $build_type | tr '[:upper:]' '[:lower:]')
+
+# make
+mkdir -p "build/$build_dir"
+pushd "build/$build_dir"
 cmake -DCMAKE_BUILD_TYPE="${build_type}" ../../
 make -j`nproc`
 popd
