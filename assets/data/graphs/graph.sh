153e9e642d7a6afa0c5892523c56a59d806e7c6a
Alan Witkowski <alan.witkowski@gmail.com>
2021-01-24 11:02:37 -0700

    graph fixes

--- assets/data/graphs/graph.sh
+++ assets/data/graphs/graph.sh
@@ -6,29 +6,33 @@ type gnuplot >/dev/null 2>&1 || {
 	exit 1;
 }
 
-# check for gawk
-type gawk >/dev/null 2>&1 || {
-	echo >&2 "gawk is not installed!";
-	exit 1;
-}
-
 # get path to log file
 log="$HOME/.local/share/choria_legacy/log/server.log"
 if [ -n "$1" ]; then
 	log="$1"
 fi
 
+
 # make temp directory
 mkdir -p gold
 mkdir -p exp
 mkdir -p kills
 
-rm gold/*
-rm exp/*
-rm kills/*
+rm -f gold/*
+rm -f exp/*
+rm -f kills/*
 
 # get list of players with character id
-players=$(grep "\[SAVE\] Saving player .* character_id=[0-9]\+ " "$log" -o | sed "s/.*Saving player //" | sed "s/\ ( character_id=/_/" | sort | uniq)
+if [ -n "$2" ]; then
+	players=`echo "$2" | tr " " "\n"`
+else
+	players=$(grep "\[SAVE\] Saving player .* character_id=[0-9]\+ " "$log" -o | sed "s/.*Saving player //" | sed "s/\ ( character_id=/_/" | sort | uniq)
+fi
+
+# return data from log file based on player name and column indexes
+function get_columns() {
+	grep "\[SAVE\] Saving player $name (" "$log" | grep "( .* )" -o | awk -v x=$1 -v y=$2 'BEGIN{FS=" "}{print $x, $y}' | tr -d '[A-Za-z=]' | awk 'BEGIN{FS=" "}{print $1/3600.0, $2}'
+}
 
 # iterate through each player and extract save data
 echo "$players" | while read player; do
@@ -41,17 +45,20 @@ echo "$players" | while read player; do
 	kills_file="kills/$player"
 
 	# get gold
-	grep "\[SAVE\] Saving player $name (" "$log" | grep "( .* )" -o | gawk 'BEGIN{FS=" "}{print $5, $4}' | tr -d '[A-Za-z=]' > "$gold_file"
+	get_columns 5 4 > "$gold_file"
 
 	# get exp
-	grep "\[SAVE\] Saving player $name (" "$log" | grep "( .* )" -o | gawk 'BEGIN{FS=" "}{print $5, $3}' | tr -d '[A-Za-z=]' > "$exp_file"
+	get_columns 5 3 > "$exp_file"
 
 	# get kills
-	grep "\[SAVE\] Saving player $name (" "$log" | grep "( .* )" -o | gawk 'BEGIN{FS=" "}{print $5, $6}' | tr -d '[A-Za-z=]' > "$kills_file"
+	get_columns 5 6 > "$kills_file"
 
 done
 
 # graph data
-gnuplot gold.gpi -p
-gnuplot exp.gpi -p
-gnuplot kills.gpi -p
+gnuplot gold.gpi -p &
+gnuplot exp.gpi -p &
+gnuplot kills.gpi -p &
+
+echo "Waiting for windows to close"
+wait
